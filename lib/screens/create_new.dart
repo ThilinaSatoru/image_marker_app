60ba0d6dc608b018de04f3a03680740f334fb4fc
Thilina Satoru <satoru.thilina@gmail.com>
2024-07-31 14:38:07 +0530

    svg scale issue fixed

--- lib/screens/create_new.dart
+++ lib/screens/create_new.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 import 'dart:typed_data';
 import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:xml/xml.dart';
@@ -25,6 +25,23 @@ class _ImageMarkerPageState extends State<ImageMarkerPage> {
   void initState() {
     super.initState();
     _loadImage();
+  }
+
+  Future<void> _pickImage() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? pickedFile =
+        await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      final File imageFile = File(pickedFile.path);
+      final Uint8List imageBytes = await imageFile.readAsBytes();
+      final ui.Image loadedImage = await decodeImageFromList(imageBytes);
+
+      setState(() {
+        image = loadedImage;
+        markers.clear();
+      });
+    }
   }
 
   Future<void> _loadImage() async {
@@ -101,6 +118,12 @@ class _ImageMarkerPageState extends State<ImageMarkerPage> {
             child: Icon(Icons.folder_open),
             heroTag: 'load',
           ),
+          SizedBox(height: 10),
+          FloatingActionButton(
+            onPressed: _pickImage,
+            child: Icon(Icons.photo_library),
+            heroTag: 'pickImage',
+          ),
         ],
       ),
     );
@@ -126,7 +149,11 @@ class _ImageMarkerPageState extends State<ImageMarkerPage> {
         offset: localPosition,
       ));
     });
+
+    // Debug logging
+    print('Marker added at: $localPosition');
   }
+
 
   void _updateMarkerPosition(Marker marker, DragUpdateDetails details) {
     setState(() {
@@ -179,8 +206,11 @@ class _ImageMarkerPageState extends State<ImageMarkerPage> {
   }
 
   Future<String> _generateSvg() async {
-    final double width = MediaQuery.of(context).size.width;
-    final double height = width * image!.height / image!.width;
+    final double originalWidth = image!.width.toDouble();
+    final double originalHeight = image!.height.toDouble();
+    final double displayedWidth = MediaQuery.of(context).size.width;
+    final double displayedHeight =
+        displayedWidth * originalHeight / originalWidth;
 
     // Convert the original image to base64
     final ByteData? originalImageData =
@@ -188,22 +218,41 @@ class _ImageMarkerPageState extends State<ImageMarkerPage> {
     final String base64OriginalImage =
         base64Encode(originalImageData!.buffer.asUint8List());
 
+    // Calculate the scale factor between the displayed size and the original size
+    final double scaleX = originalWidth / displayedWidth;
+    final double scaleY = originalHeight / displayedHeight;
+
+    // Define the offset to be added to the marker positions
+    const double offsetX = 20.0;
+    const double offsetY = 20.0;
+
     String svgString = '''
-  <svg width="$width" height="$height" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <image xlink:href="data:image/png;base64,$base64OriginalImage" width="$width" height="$height" />
+  <svg width="$originalWidth" height="$originalHeight" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <image xlink:href="data:image/png;base64,$base64OriginalImage" width="$originalWidth" height="$originalHeight" />
   ''';
 
     for (final marker in markers) {
-      final double cx = marker.offset.dx;
-      final double cy = marker.offset.dy;
+      final double cx = (marker.offset.dx * scaleX) + offsetX;
+      final double cy = (marker.offset.dy * scaleY) + offsetY;
+      final double scaledRadius = 15 * scaleX; // Scale the radius of the marker
+
+      // Debug logging
+      print('Marker position: (${marker.offset.dx}, ${marker.offset.dy})');
+      print('Scaled position: ($cx, $cy)');
+
       svgString += '''
-    <circle cx="$cx" cy="$cy" r="15" fill="red" />
+    <circle cx="$cx" cy="$cy" r="$scaledRadius" fill="red" />
     ''';
     }
 
     svgString += '</svg>';
     return svgString;
   }
+
+
+
+
+
 
   Future<void> _loadSvg() async {
     final documentsPath = await _getDocumentsPath();
@@ -237,12 +286,20 @@ class _ImageMarkerPageState extends State<ImageMarkerPage> {
     // Parse markers
     markers.clear();
     final circleElements = document.findAllElements('circle');
+    final double width = image!.width.toDouble();
+    final double height = image!.height.toDouble();
+    final Size size = Size(
+      MediaQuery.of(context).size.width,
+      MediaQuery.of(context).size.width * height / width,
+    );
 
     for (final circle in circleElements) {
       final String id = DateTime.now().millisecondsSinceEpoch.toString();
       final double cx = double.parse(circle.getAttribute('cx')!);
       final double cy = double.parse(circle.getAttribute('cy')!);
-      markers.add(Marker(id: id, offset: Offset(cx, cy)));
+      final double x = (cx / width) * size.width;
+      final double y = (cy / height) * size.height;
+      markers.add(Marker(id: id, offset: Offset(x, y)));
     }
 
     setState(() {
